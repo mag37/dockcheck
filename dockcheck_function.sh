0f14b123efd11e0e3bddd5259ffd6ffca96e5ca6
mag37 <robin.ivehult@gmail.com>
2023-01-18 19:24:29 +0100

    Update dockcheck_function.sh
    safer code, encapsulation etc

--- dockcheck_function.sh
+++ dockcheck_function.sh
@@ -9,9 +9,9 @@ dockcheck () {
     echo "No container name given, here's the list of currently running containers:" 
     docker ps --format '{{.Names}}'
   else
-    RepoUrl=$(docker inspect $1 --format='{{.Config.Image}}')
-    LocalHash=$(docker image inspect $RepoUrl --format '{{.RepoDigests}}' | sed -e 's/.*sha256/sha256/' -e 's/\]$//')
-    RegHash=$(regctl image digest --list $RepoUrl)
+    RepoUrl=$(docker inspect "$1" --format='{{.Config.Image}}')
+    LocalHash=$(docker image inspect "$RepoUrl" --format '{{.RepoDigests}}' | sed -e 's/.*sha256/sha256/' -e 's/\]$//')
+    RegHash=$(regctl image digest --list "$RepoUrl")
     if [[ "$LocalHash" != "$RegHash" ]] ; then printf "Updates available.\n" ; else printf "Already latest.\n" ; fi
   fi
 }
