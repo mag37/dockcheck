91ff4aaaa7777cccd0eacb96f8db614a95cb3bba
mag37 <robin.ivehult@gmail.com>
2024-10-02 20:44:32 +0200

    Added notification release notes url function

---
+++ notify_templates/releasenotes.sh
@@ -0,0 +1,17 @@
+### Snippet to use together with notify.sh
+# 
+# Requires a space-separated list-file of container-name and release-note-url, modify the example file "urls.list"
+# Copy urls.list and releasenotes.sh to the same directory as dockcheck.sh.
+#
+# Add the next line (uncommented) to any notification script you're using, after the "UpdToString"-variable setup
+# [ -s "$ScriptWorkDir"/releasenotes.sh ] && { source "$ScriptWorkDir"/releasenotes.sh ; UpdToString=$( releasenotes ) ; }
+
+releasenotes() {
+    for update in ${Updates[@]}; do
+        found=false
+        while read -r container url; do
+            [[ $update == $container ]] && printf "%s  ->  %s\n" "$update" "$url" && found=true
+        done < urls.list
+        [[ $found == false ]] && printf "%s\n" "$update"
+    done
+}
