d3786d6f7562a905bd77980742cca215767be1c4
mag37 <robin.ivehult@gmail.com>
2024-11-23 14:57:39 +0100

    hotfix

--- dockcheck.sh
+++ dockcheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION="v0.5.2"
+VERSION="v0.5.2.1"
 # ChangeNotes: DEPENDENCY REMINDER: now requires jq. UPDATE: Rewrite of dependency installer.
 Github="https://github.com/mag37/dockcheck"
 RawUrl="https://raw.githubusercontent.com/mag37/dockcheck/main/dockcheck.sh"
