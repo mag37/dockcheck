70a34c5821545e742dc4b2f90cb83fbee3db5286
TermeHansen <termopeten@gmail.com>
2025-07-13 13:06:58 +0200

    add documentation links

--- notify_templates/notify_HA.sh
+++ notify_templates/notify_HA.sh
@@ -1,6 +1,8 @@
 ### DISCLAIMER: This is a third party addition to dockcheck - best effort testing.
 NOTIFY_HA_VERSION="v0.1"
 #
+# This is an integration that makes it possible to send notifications via Home Assistant (https://www.home-assistant.io/integrations/notify/)
+# You need to generate a long-lived access token in Home Sssistant to be used here (https://developers.home-assistant.io/docs/auth_api/#long-lived-access-token)
 # Leave (or place) this file in the "notify_templates" subdirectory within the same directory as the main dockcheck.sh script.
 # If you instead wish make your own modifications, make a copy in the same directory as the main dockcheck.sh script.
 # Do not modify this file directly within the "notify_templates" subdirectory. Set HA_ENTITY, HA_URL and HA_TOKEN in your dockcheck.config file.
