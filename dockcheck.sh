d83f2930227c6d507377168520c4922b904983c1
mag37 <robin.ivehult@gmail.com>
2026-07-09 12:54:59 +0200

    forgot to export the variable

--- dockcheck.sh
+++ dockcheck.sh
@@ -535,7 +535,7 @@ check_image() {
 # Make required functions and variables available to subprocesses
 export -f check_image datecheck
 export Excludes_string="${Excludes[*]:-}" # Can only export scalar variables
-export t_out regbin RepoUrl DaysOld DRunUp jqbin OnlyLabel RunTimestamp RunEpoch
+export t_out regbin RepoUrl DaysOld DRunUp jqbin OnlyLabel RunTimestamp RunEpoch TestNotify
 
 # Check for POSIX xargs with -P option, fallback without async
 if (echo "test" | xargs -P 2 >/dev/null 2>&1) && [[ "$MaxAsync" != 0 ]]; then
